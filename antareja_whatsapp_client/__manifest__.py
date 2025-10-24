f75809e2b6902ec0cf95241a40700d02914edb2e
agus <agus@antarejasolusi.com>
2025-10-24 15:27:28 +0700

    antareja_whataspp_client

---
+++ antareja_whatsapp_client/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Antareja || Whatapp Client",
+    'summary': """Whatapp Client and Integration """,
+    'description': """Whatapp Client and Integration""",
+    'author': "Agus Muhammad Ramdan",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '13.0.1',
+    'depends': ['base', 'ahda_dynamic_whatsapp_client'],
+    # always loaded
+    'data': [
+        'data/whatsapp_api.xml',
+    ],
+}
