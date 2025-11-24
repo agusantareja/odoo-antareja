185c6819ecf9a9b0fb31fb8612b1e65bb0d4751f
agus <agus@antarejasolusi.com>
2025-11-24 14:58:46 +0700

    antareja_notification: 2025-11-24

---
+++ antareja_notification/models/approval_template.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, tools
+from odoo.exceptions import UserError
+from odoo.tools import *
+from odoo.tools.safe_eval import safe_eval, test_python_expr
+
+import base64
+import logging
+
+from pytz import timezone
+
+_logger = logging.getLogger(__name__)
+
+
+class ApprovalTemplateMixin(models.AbstractModel):
+    _inherit = 'approval.template.mixin'
+
+    notification_approval_id = fields.Many2one(
+        'notification.template',
+        help="Notification template used for approval notifications."
+    )
+    notification_rejection_id = fields.Many2one(
+        'notification.template',
+        help="Notification template used for reject notifications."
+    )
+    notification_approved_id = fields.Many2one(
+        'notification.template',
+        help="Notification template used for reject notifications."
+    )
