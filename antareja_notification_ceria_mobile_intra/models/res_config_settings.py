e0c2fb848e4044d7cd0426d1006dc148746380a3
agus <agus@antarejasolusi.com>
2025-11-26 15:19:51 +0700

    antareja_notification_ceria_mobile_intra: 2025-11-26

---
+++ antareja_notification_ceria_mobile_intra/models/res_config_settings.py
@@ -0,0 +1,18 @@
+from odoo import models, fields
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+    mobile_notification_application_name = fields.Char(default='intra',
+        config_parameter='antareja_notification.application_name',
+    )
+    mobile_notification_send_method = fields.Selection(
+        [('local', 'Local'), ('intra', 'Intra'),],default='intra',
+        config_parameter = 'antareja_notification.mobile_notification_send_method',
+    )
+    mobile_notification_endpoint = fields.Char(
+        config_parameter='antareja_notification.mobile_notification_endpoint'
+    )
+    mobile_notification_token = fields.Char(
+        config_parameter='antareja_notification.mobile_notification_token'
+    )
+
