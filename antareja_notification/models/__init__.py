5097fa8c5f21bccd9a80368a523a8a894cf478ec
agus <agus@antarejasolusi.com>
2025-12-05 18:14:37 +0700

    antareja_notification

--- antareja_notification/models/__init__.py
+++ antareja_notification/models/__init__.py
@@ -6,7 +6,7 @@ from . import notification_task
 from . import notification_log
 from . import approval_task
 from . import approval_task_line_mixin
-from . import approval_transaction_task
+#from . import approval_transaction_task
 from . import mail_template_internal
 from . import res_config_settings
 from . import res_users
