de5191faecf3f75a26b7a63a1e0c9019a5574122
agus <agus@antarejasolusi.com>
2025-12-10 16:39:59 +0700

    antareja_base

--- antareja_base/models/res_config_settings.py
+++ antareja_base/models/res_config_settings.py
@@ -6,11 +6,13 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     module_antareja_notification = fields.Boolean("Notification")
-    module_antareja_notification_ceria_mobile_intra = fields.Boolean("Notification Ceria Mobile Client")
     module_antareja_notification_whatsapp = fields.Boolean("Notification Whatsapp")
     module_antareja_whatsapp_migration = fields.Boolean("WhatsApp Migration")
+
     module_antareja_doa = fields.Boolean("Delegation of Authority (DoA)")
+    module_antareja_mobile_token = fields.Boolean("Mobile Token Auth")
 
-
+    module_antareja_notification_ceria_mobile_intra = fields.Boolean("Ceria Mobile Mobile Intra")
     module_ceria_mobile_notification = fields.Boolean("Ceria Mobile Notification and Approval Server")
     module_ceria_mobile_notification_test = fields.Boolean("Test Module Ceria Mobile Firebase")
+    module_ceria_mobile_notification_token = fields.Boolean("Module Ceria Mobile Token")
