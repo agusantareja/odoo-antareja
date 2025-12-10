de5fcbcafcf7881eba187db36a1067050b0b05c7
agus <agus@antarejasolusi.com>
2025-12-10 16:42:09 +0700

    antareja_notification_ceria_mobile_token

---
+++ antareja_notification_ceria_mobile_token/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Antareja || Notification Ceria Mobile Local',
+    'version': '13.0.1.0.1',
+    "category": "Extra Tools",
+    "license" : "LGPL-3",
+    'author': "Agus Muhammad Ramdan, Antareja Sinergi Sejahtera",
+    'description': """Ceria mobile yang ada di intra""",
+    'depends': ['base', 'antareja_notification_ceria_mobile_intra', 'antareja_mobile_token'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False
+}
