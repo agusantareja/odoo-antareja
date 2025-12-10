e92ed3ba5afdfc67a2c1c301b2692ba68823216e
agus <agus@antarejasolusi.com>
2025-12-10 16:42:09 +0700

    antareja_notification_ceria_mobile_token

---
+++ antareja_notification_ceria_mobile_token/models/mobile_notification_client.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+import json
+import requests
+import traceback
+
+
+class MobileNotificationClient(models.Model):
+    _inherit = "mobile.notification.client"
+
+    def prepare_send_data(self,**data):
+        data_notif = super().prepare_send_data(**data)
+        if self.to_user_id:
+            data_notif['mobile_access_token']=self.to_user_id.get_mobile_access_token(create=True)
+        return data_notif
