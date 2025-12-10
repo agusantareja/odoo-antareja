d994a249cbdfdeb986fd6ba746deddf6dad4933b
agus <agus@antarejasolusi.com>
2025-12-10 16:41:43 +0700

    antareja_notification_ceria_mobile_intra

--- antareja_notification_ceria_mobile_intra/models/res_config_settings.py
+++ antareja_notification_ceria_mobile_intra/models/res_config_settings.py
@@ -7,10 +7,11 @@ class ResConfigSettings(models.TransientModel):
     mobile_notification_application_name = fields.Char(default='intra',
         config_parameter='antareja_notification.application_name',
     )
-    mobile_notification_send_method = fields.Selection(
-        [('local', 'Local'), ('intra', 'Intra'),],default='intra',
-        config_parameter = 'antareja_notification.mobile_notification_send_method',
-    )
+
+    # mobile_notification_send_method = fields.Selection(
+    #     [('local', 'Local'), ('intra', 'Intra'),],default='intra',
+    #     config_parameter = 'antareja_notification.mobile_notification_send_method',
+    # )
     mobile_notification_endpoint = fields.Char(
         config_parameter='antareja_notification.mobile_notification_endpoint'
     )
@@ -18,3 +19,4 @@ class ResConfigSettings(models.TransientModel):
         config_parameter='antareja_notification.mobile_notification_token'
     )
     module_antareja_notification_ceria_mobile_local = fields.Boolean("Local Ceria Mobile Notification")
+    module_antareja_notification_ceria_mobile_token = fields.Boolean("Notification Ceria Mobile Token")
