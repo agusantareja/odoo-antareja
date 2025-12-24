7f4900b964ef43e28cc55606f3198806120febd2
agus <agus@antarejasolusi.com>
2025-12-24 17:16:13 +0700

    antareja_ocr_attachment

---
+++ antareja_ocr_attachment/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Antareja || OCR Attachment',
+    'version': '13.0.1.0.1',
+    "category": "Extra Tools",
+    "license" : "LGPL-3",
+    'author': "Agus Muhammad Ramdan, Antareja Sinergi Sejahtera",
+    'description': "Convert Attacment PDF to Text",
+    'depends': ['base'],
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False
+}
