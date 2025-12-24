c7e4470685d9a4f1eb5898ed298c703986fd37de
agus <agus@antarejasolusi.com>
2025-12-24 17:15:16 +0700

    antareja_mobile_token

--- antareja_mobile_token/models/mobile_access_token.py
+++ antareja_mobile_token/models/mobile_access_token.py
@@ -25,9 +25,79 @@ class MobileAccessToken(models.Model):
     user_id = fields.Many2one('res.users', string='User')
     expires = fields.Datetime('Expires')
 
-    def _get_access_token(self, user_id=None, create=False):
+    def token_expires_in(self):
+        return int(self.env['ir.config_parameter'].sudo().get_param('antareja_mobile_token.expires_in')) or (
+                60 * 60 * 24)
+
+    def generate_token(self,user_id):
+        vals = {
+            'user_id': user_id,
+            'token': oauthlib_common.generate_token(),
+        }
+        expires_in = self.token_expires_in()
+        expires = datetime.now() + timedelta(seconds=expires_in)
+        vals['expires'] = expires.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+        return vals
+
+    def get_jwt_secret(self):
+        return self.env['ir.config_parameter'].sudo().get_param('jwt.secret')
+
+    def get_jwt_algorithm(self):
+        return 'HS256'
+
+    def generate_jwt_token(self,uid,login):
+        jwt_secret = self.get_jwt_secret()
+        if not jwt_secret:
+            return None
+        expires_in = self.token_expires_in()
+        try:
+            import jwt
+            payload = {
+                'uid': uid,
+                'login': login,
+                'exp': datetime.utcnow() + timedelta(seconds=expires_in)
+            }
+            return jwt.encode(payload, self.get_jwt_secret(), algorithm=self.get_jwt_algorithm())
+        except ImportError:
+            return None
+
+
+    def validate_jwt(self,token):
+        try:
+            import jwt
+            from jwt import ExpiredSignatureError, InvalidTokenError
+            try:
+                payload = jwt.decode(
+                    token,
+                    self.get_jwt_secret(),
+                    algorithms=[self.get_jwt_algorithm()]
+                )
+                return payload
+            except ExpiredSignatureError:
+                return None
+            except InvalidTokenError:
+                return None
+        except ImportError:
+            return None
+
+
+    def validate(self,token):
+        jwt_data = self.validate_jwt(token)
+        if jwt_data:
+            return jwt_data
+        res = self.sudo().search([('token', '=', token)], order='id DESC', limit=1)
+        if res.is_expired():
+            return None
+        return {
+            'uid': res.user_id.id,
+            'login': res.user_id.login,
+        }
+
+    def _get_access_token(self, user_id=None, create=False,login=None):
         if not user_id:
             user_id = self.env.user.id
+        if not login:
+            login = self.user_id.sudo().browse(user_id).login
 
         access_token = self.sudo().search(
             [('user_id', '=', user_id)], order='id DESC', limit=1)
@@ -36,14 +106,7 @@ class MobileAccessToken(models.Model):
             if access_token.is_expired():
                 access_token = None
         if not access_token and create:
-            vals = {
-                'user_id': user_id,
-                'token': oauthlib_common.generate_token(),
-            }
-            value = int(self.env['ir.config_parameter'].sudo().get_param('antareja_mobile_token.expires_in')) or (60*60*24)
-            if value:
-                expires = datetime.now() + timedelta(seconds=value)
-                vals['expires'] = expires.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+            vals = self.generate_jwt_token(user_id,login) or self.generate_token(user_id)
             access_token = self.sudo().create(vals)
             # we have to commit now
             # be called before we finish current transaction.
