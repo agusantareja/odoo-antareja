02cf2e5e250adc44928e58609ffd65a184eb45d5
agus <agus@antarejasolusi.com>
2025-12-24 17:16:08 +0700

    antareja_notification_minute_ocr_

---
+++ antareja_minute_ocr_queue/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Antareja || Minute OCR Queue',
+    'version': '13.0.1.0.1',
+    "category": "Extra Tools",
+    "license" : "LGPL-3",
+    'author': "Agus Muhammad Ramdan, Antareja Sinergi Sejahtera",
+    'description': "Convert Attacment PDF to Text using Queue Job",
+    'depends': ['antareja_minute_ocr','queue_job'],
+    'data': [
+        'data/queue_job_function.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False
+}
