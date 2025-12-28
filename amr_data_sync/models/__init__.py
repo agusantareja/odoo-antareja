58e7252e239ffe1af433903322b74519c06fe2bb
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2025-12-28 22:12:37 +0700

    amr_data_sync: v13.0.0.0.0

---
+++ amr_data_sync/models/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from . import data_mapping
+from . import data_lookup
+
+from . import data_sync
+from . import data_sync_server
+from . import data_sync_strategy
+from . import data_sync_cron
+from . import data_sync_exclude
+
+from . import res_company
+from . import res_users
+from . import res_partner
