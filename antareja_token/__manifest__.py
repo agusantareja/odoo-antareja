ab8f8ffb9fd7858cac087d49fb513690017fd6d4
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2025-12-28 22:16:23 +0700

    antareja_token

---
+++ antareja_token/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "JWT",
+    'summary': """
+        JWT
+        """,
+    'description': """
+    """,
+    'author': "Agus Muhammad Ramdan",
+    'website': "http://www.yourcompany.com",
+    'category': 'API',
+    'version': '13.0.0.0.0',
+    'depends': ['base', 'antareja_base', ],
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        # 'views/res_user_view.xml',
+        'views/res_config_settings_views.xml',
+        #'wizard/create_token_wizard.xml',
+    ],
+}
---
+++ antareja_token/models/antareja_token.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from odoo import models, fields, api
+from datetime import datetime, timedelta
+import jwt
+import time
+from jwt import ExpiredSignatureError, InvalidTokenError, InvalidAudienceError
+from odoo.exceptions import AccessDenied
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+
+_logger = logging.getLogger(__name__)
+
+
+# try:
+#     from oauthlib import common as oauthlib_common
+# except ImportError:
+#     _logger.warning(
+#         'OAuth library not found. If you plan to use it, '
+#         'please install the oauth library from '
+#         'https://pypi.python.org/pypi/oauthlib')
+
+
+class AccessToken(models.Model):
+    _name = 'antareja.token'
+    active = fields.Boolean(default=True)
+    token = fields.Char('Token', required=True)
+    user_id = fields.Many2one('res.users', string='User')
+    expires = fields.Integer('Expires (Epoc)')
+    retention = fields.Integer('Retention (Epoc)')
+
+    def get_expires_in(self):
+        return int(self.env['ir.config_parameter'].sudo().get_param('antareja_token.expires_in')) or (60 * 60 * 24)
+
+    def get_retention_in(self):
+        return int(self.env['ir.config_parameter'].sudo().get_param('antareja_token.retention_in')) or (60 * 60 * 4)
+
+    def get_secret(self):
+        return self.env['ir.config_parameter'].sudo().get_param('antareja_token.secret')
+
+    def get_issuer(self):
+        return self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+
+    def get_audience(self):
+        return self.env['ir.config_parameter'].sudo().get_param('antareja.application_name')
+
+    def get_algorithm(self):
+        return 'HS256'
+
+    def login(self, uid):
+        user = self.env["res.users"].sudo().browse(uid)
+        expires_in = self.get_expires_in()
+        access_token, payload = self.generate_token(user, expires_in=expires_in)
+        refresh_token = self.create_refresh_token(user)
+        return {
+            "access_token": access_token,
+            "refresh_token": refresh_token
+        }
+
+    def create_refresh_token(self, user):
+        Refresh = self.env["antareja.refresh.token"].sudo()
+
+        token = Refresh.generate_token()
+        expires_in = (60 * 60 * 24*14)
+        expire = int(time.time()) + expires_in
+        expires_at = datetime.utcnow() + timedelta(seconds=expires_in)
+
+        Refresh.create({
+            "token": token,
+            "user_id": user.id,
+            "expires_at": expires_at,
+        })
+        payload = {
+            'token': token,
+            'data': f"{user.id}-{user.email}-{time.time()}-{self.env.cr.dbname}",
+            'aud': self.get_audience(),
+            'iss': self.get_issuer(),
+            'exp': expire
+        }
+        return jwt.encode(
+            payload,
+            self.get_secret(),
+            algorithm=self.get_algorithm()
+        )
+
+    def generate_token(self, user, expires_in=None):
+        jwt_secret = self.get_secret()
+        if not jwt_secret:
+            return None, None
+        expires_in = expires_in or self.get_expires_in()
+        expire = int(time.time()) + expires_in
+        payload = {
+            'uid': user.id,
+            'sub': user.login,
+            'email': user.email,
+            'db': self.env.cr.dbname,
+            'iss': self.get_issuer(),
+            'aud': self.get_audience(),
+            'exp': expire
+        }
+        return jwt.encode(
+            payload,
+            self.get_secret(),
+            algorithm=self.get_algorithm()
+        ), payload
+
+    def validate(self, token):
+        try:
+            payload = jwt.decode(
+                token,
+                self.get_secret(),
+                issuer=self.get_issuer(),
+                audience=self.get_audience(),
+                algorithms=[self.get_algorithm()],
+                options={
+                    "require": ["exp", "iss", "aud"]
+                }
+            )
+            return payload
+        except InvalidAudienceError:
+            return None
+        except ExpiredSignatureError:
+            return None
+        except InvalidTokenError:
+            return None
+
+    def create_access_token(self, user, retention_in=None):
+        retention_in = retention_in or self.get_retention_in()
+        token, payload = self.generate_token(user)
+        vals = {
+            'user_id': user.id,
+            'token': token,
+            'expires': int(payload['exp']),
+            'retention': time.time() + retention_in,
+        }
+        access_token = self.sudo().create(vals)
+        # we have to commit now
+        # be called before we finish current transaction.
+        self._cr.commit()
+        return access_token
+
+    def get_access_token(self, user=None, create=False):
+        user = user or self.env.user
+        epoch_time = time.time()
+        access_token = self.sudo().search(
+            [('user_id', '=', user.id), ('expires', '>', epoch_time)], order='expires DESC', limit=1
+        ) or create and self.create_access_token(user)
+
+        return access_token and access_token.token or None
+
+    def cron_cleanup_tokens(self):
+        epoch_time = time.time()
+        tokens = self.sudo().search([('retention', '<', epoch_time)])
+        tokens.sudo().unlink()
