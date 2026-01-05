56dcd3d32e33025defffa16d605c18ba17837cb6
agus <agus@antarejasolusi.com>
2026-01-05 09:10:57 +0700

    antareja_integration_app_hr_cerindocorp

---
+++ antareja_integration_app_hr_cerindocorp/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Integration ERP Cerindo Corp",
+    'summary': """Application Server Integration for ERP Cerindo Corp""",
+    'description': """
+        Application Server Integration for ERP Cerindo Corp""",
+    'author': "Agus Muhammad Ramdan",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '13.0.1.0.0.1',
+    'depends': ['base', 'mail', 'antareja_integration'],
+    # always loaded
+    'data': [
+        'data/application_server_data.xml',
+
+    ],
+}
