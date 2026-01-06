2fde925e94a010896cac0c3b853c905fd8376e2c
agus <agus@antarejasolusi.com>
2026-01-06 17:04:09 +0700

    antareja_token_client 13.0.0.0.1

---
+++ antareja_token_client/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Token Client",
+    'summary': """
+        Token Client
+    """,
+    'description': """
+    """,
+    'author': "Agus Muhammad Ramdan",
+    'website': "http://www.yourcompany.com",
+    'category': 'API',
+    'version': '13.0.0.0.1',
+    'depends': ['base', 'antareja_base','antareja_token', 'cni_api' ,'rest_api'],
+    # always loaded
+    'data': [],
+}
