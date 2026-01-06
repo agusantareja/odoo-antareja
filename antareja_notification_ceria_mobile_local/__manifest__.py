76691251946255de99e13c4ec82b64e032e2a1bd
agus <agus@antarejasolusi.com>
2026-01-06 12:58:42 +0700

    antareja_notification_ceria_mobile_local 13.0.1.0.2

--- antareja_notification_ceria_mobile_local/__manifest__.py
+++ antareja_notification_ceria_mobile_local/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Antareja || Notification Ceria Mobile Local',
-    'version': '13.0.1.0.1',
+    'version': '13.0.1.0.2',
     "category": "Extra Tools",
     "license" : "LGPL-3",
     'author': "Agus Muhammad Ramdan, Antareja Sinergi Sejahtera",
--- antareja_notification_ceria_mobile_local/models/__init__.py
+++ antareja_notification_ceria_mobile_local/models/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from . import notification_mobile_template
-from . import mobile_approval_client
-from . import ceria_mobile_approval
-from . import res_config_settings
+# from . import mobile_approval_client
+# from . import ceria_mobile_approval
