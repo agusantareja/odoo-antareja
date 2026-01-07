0ac16e2d47d79e6ccf04ba03d03671622fdc9551
agus <agus@antarejasolusi.com>
2026-01-07 16:04:13 +0700

    antareja_sync_api

---
+++ antareja_sync_api/models/ir_model.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, _
+
+
+class IrModel(models.Model):
+    _inherit = 'ir.model'
+
+    @api.model
+    def is_read_sync_api(self):
+
+        return True
+
+    def readable_fields(self, fields):
+        env = self.env
+        model_obj = self.env[self.model]
+        if not fields:
+            _fields = model_obj._fields
+            exclude_fields = []
+            exclude_fields.extend(env['mail.thread']._fields.keys())
+            exclude_fields.extend(env['mail.activity.mixin']._fields.keys())
+            exclude_fields.extend(env['mail.blacklist']._fields.keys())
+            fs = model_obj.check_field_access_rights('read', None)
+            fields = [name for name in fs if
+                      name not in exclude_fields and not name.startswith("message_") and not isinstance(
+                          _fields[name].compute, bool)]
+        return fields
