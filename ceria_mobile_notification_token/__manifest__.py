133d74152d1c3dcdcce0558fbf479530d26e583c
agus <agus@antarejasolusi.com>
2026-01-07 09:09:33 +0700

    ceria_mobile_notification

---
+++ ceria_mobile_notification_token/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name':"Ceria Mobile Notification Token",
+    'version':'13.0.1.0.1',
+    'depends':['base','ceria_mobile_notification','antareja_token'],
+    'website':"",
+    'description':"""Ceria Mobile Notification Token""",
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+}
