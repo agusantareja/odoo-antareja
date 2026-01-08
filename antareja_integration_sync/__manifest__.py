29f10a4e8d40b90259d1322a8c99a7c04a0cb79b
agus <agus@antarejasolusi.com>
2026-01-08 13:09:10 +0700

    antareja_integratiion_sync

--- antareja_integration_sync/__manifest__.py
+++ antareja_integration_sync/__manifest__.py
@@ -1,32 +1,29 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Antarja || Approval Manager",
+    'name': "Antarja || Integartion Sync",
 
-    'summary': """Integartion Approval Manager""",
+    'summary': """Integartion Sync""",
 
     'description': """
-        Integartion Approval Manager
+        Integartion Sync
     """,
 
-    'author': "IT Antareja",
+    'author': "Agus Muhammad Ramdan",
     'website': "http://www.yourcompany.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'category': 'Tools',
     'version': '13.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': [
         'base',
         'antareja_base',
+        'amr_data_sync',
     ],
     # always loaded
     'data': [
-        'data/cron.xml',
+        'views/server_sync_views.xml',
         'views/application_server_auth_views.xml',
-        'views/menuitem.xml',
     ],
 
 }
