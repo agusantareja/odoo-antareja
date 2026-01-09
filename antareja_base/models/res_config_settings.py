ae82fbc9595465a3525d29629fb251234203fddf
agus <agus@antarejasolusi.com>
2026-01-09 17:43:05 +0700

    antareja_base 13.0.0.0.7

--- antareja_base/models/res_config_settings.py
+++ antareja_base/models/res_config_settings.py
@@ -7,6 +7,8 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
     antareja_application_name = fields.Char(config_parameter='antareja.application_name')
 
+    module_antareja_sync_api = fields.Boolean("Sync Service API")
+    module_antareja_report = fields.Boolean("Report Store")
     module_antareja_integration = fields.Boolean("Application Integration")
 
     module_antareja_notification = fields.Boolean("Notification")
@@ -14,13 +16,14 @@ class ResConfigSettings(models.TransientModel):
     module_antareja_whatsapp_migration = fields.Boolean("WhatsApp Migration")
 
     module_antareja_approval_manager = fields.Boolean("Approval Manager")
-
+    module_antareja_approval_hr_employee_hierarchy = fields.Boolean("Employee Approval Hierarchy")
     module_antareja_token = fields.Boolean("JWT Token")
 
     module_antareja_doa = fields.Boolean("Delegation of Authority (DoA)")
 
+    module_antareja_approval_ceria_mobile_intra = fields.Boolean("Ceria Apprval Mobile Intra")
     module_antareja_notification_ceria_mobile_intra = fields.Boolean("Ceria Mobile Mobile Intra")
 
-    module_ceria_mobile_notification = fields.Boolean("Ceria Mobile Notification and Approval Server")
-    module_ceria_mobile_notification_test = fields.Boolean("Test Module Ceria Mobile Firebase")
-    module_ceria_mobile_notification_token = fields.Boolean("Module Ceria Mobile Token")
+    # module_ceria_mobile_notification = fields.Boolean("Ceria Mobile Notification Server")
+    # module_ceria_mobile_notification_test = fields.Boolean("Test Module Ceria Mobile Firebase")
+    # module_ceria_mobile_notification_token = fields.Boolean("Module Ceria Mobile Token")
