6d617c5f8d7bb344d561bc24d9f73dd0f967683b
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-01-11 21:13:07 +0700

    antareja_base: v13.0.0.0.8

--- antareja_base/models/__init__.py
+++ antareja_base/models/__init__.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from . import res_users
+from . import ir_config_parameter
+from . import application_server
+from . import application_server_auth
+from . import application_server_path
+from . import api_call
 from . import approval_transaction
 from . import approval_transaction_view
 from . import approval_audit_log
 from . import abstract_approval
-from . import approval_task
 from . import approval_transaction_task
 from . import approval_template
 from . import approval_instance
 from . import approval_task_line
 from . import approval_instance_able
-
+from . import approval_task
+from . import delay_call
+from . import res_config_settings
