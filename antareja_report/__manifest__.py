4506f98f0508d1d5705d0ede080a96f99253a66d
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-01-12 20:53:45 +0700

    antareja_report

---
+++ antareja_report/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Antareja Report Generate Store",
+    'summary': """
+        Report Generate Store
+        """,
+    'description': """
+        Antareja report generate secara background dan disimpan di server
+    """,
+    'author': "Agus Muhammad Ramdan",
+    'website': "http://agus.ramdan.tech",
+    'category': 'tools',
+    'version': '13.0.0.0.0',
+    'depends': ['base', 'web', 'antareja_base', 'amr_report', 'amr_report_xls'],
+    # always loaded
+    'data': [],
+}
