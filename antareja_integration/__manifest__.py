90976c77990a1c78da6021e5ae492e3de7d96edb
agus <agus@antarejasolusi.com>
2026-01-21 16:26:04 +0700

    antareja_integration

--- antareja_integration/__manifest__.py
+++ antareja_integration/__manifest__.py
@@ -1,26 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Antarja || Approval Manager",
+    'name': "Antarja || Integration",
 
-    'summary': """Integartion Approval Manager""",
+    'summary': """Integartion Integartion""",
 
     'description': """
-        Integartion Approval Manager
+        Integartion 
     """,
 
     'author': "IT Antareja",
     'website': "http://www.yourcompany.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '13.0.1.0.0',
+    'category': 'Tools',
+    'version': '16.0.1.1.0',
 
     # any module necessary for this one to work correctly
     'depends': [
         'base',
-        'antareja_base',
+        'amr_jsonrpc',
+        'antareja_base'
     ],
     # always loaded
     'data': [
--- antareja_integration/models/application_server_auth.py
+++ antareja_integration/models/application_server_auth.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import requests
-import datetime
-import jwt
-import time
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
-import json
-import traceback
+from odoo.addons.amr_jsonrpc import jsonrpc, rest
+import requests
+import jwt
+import time
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -18,8 +16,8 @@ class ApplicationServerAuth(models.Model):
 
     auth_type = fields.Selection(selection_add=[
         ('jwt-odoo-rcp', 'JWT Odoo RCP'),
-        ('odoo-rcp',),
-    ], default='rest-token')
+        ('jwt-rest-token', 'JWT rest-token'),
+    ])
 
     rest_refresh = fields.Char()
 
@@ -57,32 +55,43 @@ class ApplicationServerAuth(models.Model):
     def rest_login(self, login, password, **kwargs):
         rec = self.ensure_one()
         url = rec.rest_url(rec.rest_login_path())
-        data = {'grant_type': 'password', 'username': login, 'password': password}
-        response = requests.post(url, data=data)
-        response.raise_for_status()
-        json_result = response.json()
-        rest_token = json_result.get('access_token')
-        rest_refresh = json_result.get('refresh_token')
-        if rest_token:
-            rec.rest_token = rest_token
-        if rest_refresh:
-            rec.rest_refresh_token = rest_refresh
+        rest_token, rest_refresh = rest.request_token(url, login, password, **kwargs)
+        rec.rest_token = rest_token or rec.rest_token
+        rec.rest_refresh_token = rest_refresh or rec.rest_refresh_token
         return rest_token
+
+    def rest_headers(self, headers=None):
+        if self.rest_token_in == 'basic':
+            return self.rest_basic_header(headers)
+        if self.rest_token_in == 'bearer':
+            return self.rest_bearer_header(headers)
+        if self.rest_token_in == 'header':
+            if headers is None:
+                headers = {}
+            headers[self.rest_token_key] = self.get_rest_token()
+        return headers
+
+    def rest_basic_header(self, headers=None):
+        username, password = self.get_username_password()
+        return rest.basic_auth_header(username, password, headers)
+
+    def rest_bearer_header(self, headers=None):
+        return rest.bearer_auth_header(self.ensure_token(), headers)
+
+    def rest_profile(self):
+        rec = self.ensure_one()
+        url = rec.rest_url(rec.rest_profile_path())
+        response = requests.get(url, rec.rest_bearer_header())
+        response.raise_for_status()
+        return response.json()
 
     def rest_post_refresh(self, refresh_token=None, **kwargs):
         rec = self.ensure_one()
         refresh_token = refresh_token or rec.rest_refresh
         url = rec.rest_url(rec.rest_login_path())
-        data = {'grant_type': 'password', 'refresh_token': refresh_token}
-        response = requests.post(url, data=data)
-        response.raise_for_status()
-        json_result = response.json()
-        rest_token = json_result.get('access_token')
-        rest_refresh = json_result.get('refresh_token')
-        if rest_token:
-            rec.rest_token = rest_token
-        if rest_refresh:
-            rec.rest_token_refresh = rest_refresh
+        rest_token, rest_refresh = rest.request_refresh_token(url, refresh_token)
+        rec.rest_token = rest_token or rec.rest_token
+        rec.rest_token_refresh = rest_refresh or rec.rest_token_refresh
         return rest_token
 
     def ensure_token(self):
@@ -93,8 +102,8 @@ class ApplicationServerAuth(models.Model):
 
     def jsonrpc_authenticate(self):
         rec = self.ensure_one()
-        db, uid, token = None, None, rec.ensure_token()
         if rec.auth_type == 'jwt-odoo-rcp':
+            db, uid, token = None, None, rec.ensure_token()
             if rec.is_jwt():
                 payload = jwt.decode(token, options={"verify_signature": False})
                 db = payload.get("db", None)
@@ -110,10 +119,69 @@ class ApplicationServerAuth(models.Model):
             if not db or not uid:
                 raise UserError(_("Invalid JWT Token"))
         else:
-            db, uid, token = super(ApplicationServerAuth, self).jsonrpc_authenticate()
+            db, username, password = self.get_odoo_db_username_password()
+            db, uid, token = jsonrpc.authenticate(self.get_jsonrpc_url(), db, username, password)
         return db, uid, token
 
+    def jsonrpc_execute_kw(self, model, method, args, kw=None, db=None, uid=None, password=None):
+        """Deprecated gunakan remote_model_object"""
+        return jsonrpc.execute_kw(self.get_jsonrpc_url(), model, method, args, kw=kw, db=db, uid=uid, password=password)
+
+    def jsonrpc_call(self, model, method, args, kw=None, db=None, uid=None, password=None):
+        """Deprecated gunakan remote_model_object"""
+        if not db or not uid or not password:
+            db, uid, password = self.jsonrpc_authenticate()
+        return jsonrpc.execute_kw(self.get_jsonrpc_url(), model, method, args, kw=kw, db=db, uid=uid, password=password)
+
     def action_login(self):
-        for rec in self:
-            rec.rest_login(rec.odoo_username, rec.odoo_password)
-        return True
+        self.ensure_one()
+        try:
+            username, password = self.get_odoo_username_password()
+            self.rest_login(username, password)
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'params': {
+                    'title': 'Info',
+                    'message': _("Token successful."),
+                    'type': 'info',
+                }
+            }
+        except Exception as e:
+            raise UserError(_("Get Token failed: %s") % str(e))
+
+    def get_auth_config(self, config=None):
+        config= {}
+        auth_type = self.auth_type
+        token_key = self.rest_token_key
+        access_token = self.rest_token
+        refresh_token = self.rest_refresh
+        token_endpoint_url = self.rest_url(self.rest_login_path())
+        if self.auth_type in ['jwt-rest-token','rest-token']:
+            auth_type = self.rest_token_in
+        if self.auth_type in ['rest-token', 'rest-token']:
+            endpoint_url = self.get_jsonrpc_url()
+        else:
+            endpoint_url = self.rest_endpoint_url()
+        db, uid, username, password = self.get_db_uid_username_password()
+        config.update({
+            'db': db,
+            'uid': uid,
+            'username': username,
+            'password': password,
+            'auth_mode': auth_type,
+            'token_key': token_key,
+            'access_token': access_token,
+            'refresh_token': refresh_token,
+            'endpoint_url':endpoint_url,
+            'token_endpoint_url': token_endpoint_url
+        })
+        return config
+
+    def remote_model_object(self, external_model, **kwargs):
+        config = self.get_auth_config()
+        config.update(kwargs)
+        if self.auth_type in ['jwt-rest-token','rest-token']:
+            return rest.model_object(external_model, **config)
+        else:
+            return jsonrpc.model_object(external_model, **config)
