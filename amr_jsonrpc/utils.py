529e576e4261096f362447a38ec51b06b672efc2
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-01-23 06:13:51 +0700

    amr_jsonrpc

--- amr_jsonrpc/utils.py
+++ amr_jsonrpc/utils.py
@@ -33,7 +33,6 @@ def savepoint(_func=None, *,
                 with self.env.cr.savepoint():
                     return func(self, *args, **kwargs)
             except Exception as e:
-                self.env.cache.invalidate()
                 logger.warning("[SAFEPOINT] %s failed: %s", func.__name__, e, exc_info=True)
                 if rethrow:
                     raise
@@ -109,7 +108,6 @@ def call_with_savepoint(self, method_name, args=None, kwargs=None, logger=_logge
         with self.env.cr.savepoint():
             return method(*final_args, **final_kwargs)
     except Exception as e:
-        self.env.cache.invalidate()
         logger.warning("[SAFEPOINT] %s failed: %s", method_name, e, exc_info=True)
         if rethrow:
             raise
