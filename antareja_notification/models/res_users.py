230b51ea3290b1439f6bb6174bb6c12d914b1b70
agus <agus@antarejasolusi.com>
2026-01-26 15:23:56 +0700

    antaeja_notification

--- antareja_notification/models/res_users.py
+++ antareja_notification/models/res_users.py
@@ -15,14 +15,20 @@ class ResUsers(models.Model):
         #v16
         odoobot = self.env.ref('base.partner_root')
         for user in self:
-            if self._uid != user.id:
-                self = self.with_user(user)
-            channel_info = self.env['mail.channel'].channel_get([odoobot.id])
-            channel = self.env['mail.channel'].browse(channel_info['id'])
-            channel.message_post(
-                body=_(message),
-                author_id=odoobot.id,
-                message_type='comment',
-                subtype_xmlid='mail.mt_comment',
-            )
-            user.sudo().odoobot_state = 'onboarding_emoji'
+            try:
+                with self.env.cr.savepoint():
+                    if self._uid != user.id:
+                        self = self.with_user(user)
+                    channel_info = self.env['mail.channel'].channel_get([odoobot.id])
+                    channel = self.env['mail.channel'].browse(channel_info['id'])
+                    channel.message_post(
+                        body=_(message),
+                        author_id=odoobot.id,
+                        message_type='comment',
+                        subtype_xmlid='mail.mt_comment',
+                    )
+                    user.sudo().odoobot_state = 'onboarding_emoji'
+
+            except Exception:
+                _logger.info('User #%i %s error chanel.',user.id, user.name)
+
