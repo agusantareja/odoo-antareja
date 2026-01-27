5422fccae878007430f6d7eda69c1fc2fcbcfe43
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-01-27 23:29:05 +0700

    antareja_integration_sync: 13.0.1.0.2

--- antareja_integration_sync/models/__init__.py
+++ antareja_integration_sync/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from . import data_sync_server
-from . import application_server_auth
-from . import application_server_path
+# from . import application_server_auth
+# from . import application_server_path
