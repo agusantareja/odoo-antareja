b6ea3c744e32beea52f3923f1a9bdfe396acdae9
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-01-27 23:27:36 +0700

    antareja_notification_admin_user 13.0.1.0.2

--- antareja_notification_admin_user/models/__init__.py
+++ antareja_notification_admin_user/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import approval_task
+from . import res_users
--- antareja_notification_admin_user/models/approval_task.py
+++ antareja_notification_admin_user/models/approval_task.py
@@ -9,6 +9,10 @@ _logger = logging.getLogger(__name__)
 class ApprovalTask(models.Model):
     _inherit = 'approval.task'
 
+    def get_users_for_approval(self,**kwargs):
+        users = super(ApprovalTask, self).get_users_for_approval(**kwargs)
+        return users and users.filtered(lambda u: not u.admin_user)
+
     def get_users_for_notification(self,**kwargs):
         users = super(ApprovalTask, self).get_users_for_notification(**kwargs)
         return users and users.filtered(lambda u: not u.admin_user)
