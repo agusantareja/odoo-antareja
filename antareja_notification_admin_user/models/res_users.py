ae8e062db3b289e4c8ef4c276b0a1d6c3095641b
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-01-27 23:27:36 +0700

    antareja_notification_admin_user: 13.0.1.0.2

---
+++ antareja_notification_admin_user/models/res_users.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api, tools
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    def get_users_for_notification(self, company=None):
+        if not self:
+            return
+        return super(ResUsers,self.filtered(lambda u: not u.admin_user)).get_users_for_notification(company=company)
+
+    def get_users_for_approval(self, company=None):
+        if not self:
+            return
+        return super(ResUsers,self.filtered(lambda u: not u.admin_user)).get_users_for_approval(company=company)
