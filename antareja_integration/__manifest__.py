b3bf921965f1391ad43b549991adcbdea15e867b
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-01-29 02:36:19 +0700

    antareja_integration: 13.0.1.1.2

--- antareja_integration/__manifest__.py
+++ antareja_integration/__manifest__.py
@@ -12,7 +12,7 @@
     'website': "http://www.yourcompany.com",
 
     'category': 'Tools',
-    'version': '13.0.1.1.1',
+    'version': '13.0.1.1.2',
 
     # any module necessary for this one to work correctly
     'depends': [
