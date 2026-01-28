64178e789e19e82089e4bf6eee4ef74595a4c5fc
agus <agus@antarejasolusi.com>
2026-01-28 17:17:41 +0700

    amr_data_sync_event

---
+++ amr_data_sync_event/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnuorg/licenses/agpl.html).
+
+{
+    'name': "Internal Data Sync by Event",
+    'summary': """ 
+    Internal Data Sync by Event
+        """,
+    'description': """
+    """,
+    'author': "Agus Muhammad Ramdan",
+    "license": "AGPL-3",
+    'website': "https://agus.ramdan.tech",
+    'category': 'tools',
+    'version': '13.0.0.0.0',
+    'depends': ['base','amr_jsonrpc','amr_data_sync'],
+    # always loaded
+    'data': [
+        'views/data_event_views.xml',
+        'views/server_sync_views.xml',
+        'views/menuitem.xml'
+    ],
+}
