b47ed94b14168078d67d58b367d7f2b6f3e4c23c
agus <agus@antarejasolusi.com>
2026-01-29 13:44:36 +0700

    antareja_integration_sync: 13.0.2.0.0

--- antareja_integration_sync/__manifest__.py
+++ antareja_integration_sync/__manifest__.py
@@ -12,7 +12,7 @@
     'website': "http://www.agus.ramdan.tech",
 
     'category': 'Tools',
-    'version': '13.0.1.0.2',
+    'version': '13.0.2.0.0',
 
     # any module necessary for this one to work correctly
     'depends': [
