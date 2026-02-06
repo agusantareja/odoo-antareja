f58cb9ff9031c78427459d9226e8e7433b2e278f
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-02-06 20:15:31 +0700

    antareja_base 16.0.1.0.3

--- antareja_base/__manifest__.py
+++ antareja_base/__manifest__.py
@@ -17,7 +17,7 @@
     'author': "Agus Muhammad Ramdan",
     'website': "http://agus.ramdan.tech",
     'category': 'Base',
-    'version': '16.0.1.0.2',
+    'version': '16.0.1.0.3',
     # any module necessary for this one to work correctly
     'depends': ['base', 'base_setup', 'mail'],
     # always loaded
