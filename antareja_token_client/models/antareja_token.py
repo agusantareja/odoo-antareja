0d3e6c1cfdb80da0cfe83a6d83b9f5ec8070ed1b
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-02-07 15:54:47 +0700

    antareja_token_client 13.0.0.0.2

--- antareja_token_client/models/antareja_token.py
+++ antareja_token_client/models/antareja_token.py
@@ -12,31 +12,32 @@ class AccessToken(models.Model):
 
     def validate(self, access_token, refresh_token=False):
         try:
-            result = super(AccessToken,self).validate(access_token,refresh_token=refresh_token)
+            result = super(AccessToken, self).validate(access_token, refresh_token=refresh_token)
         except Exception as e:
-            result=None
-        if result and not refresh_token:
+            result = None
+        if not result and not refresh_token:
             access_token_data = self.env['oauth.access_token'].sudo().search([('token', '=', access_token)], order='id DESC', limit=1)
             if access_token_data._get_access_token_google(employee_id=access_token_data.employee_id.id) != access_token:
-                user = access_token_data.employee_id
-                epoch_unix = None
-                if access_token_data.expires:
-                    epoch_timestamp = fields.Datetime.to_datetime(access_token_data.expires).timestamp()
-                    epoch_unix = int(epoch_timestamp)
-                result = {
-                    'uid': user.id,
-                    'login': user.login,
-                    'sub':user.login,
-                    'email':user.email,
-                    'exp':epoch_unix,
-                    'token': access_token,
-                }
+                user = access_token_data.employee_id.user_id
+                if user:
+                    epoch_unix = None
+                    if access_token_data.expires:
+                        epoch_timestamp = fields.Datetime.to_datetime(access_token_data.expires).timestamp()
+                        epoch_unix = int(epoch_timestamp)
+                    result = {
+                        'uid': user.id,
+                        'login': user.login,
+                        'sub': user.login,
+                        'email': user.email,
+                        'exp': epoch_unix,
+                        'token': access_token,
+                    }
         return result
 
-    def client_token_validation(self,token):
+    def client_token_validation(self, token):
         # client_api
         client_id = self.env['client.api'].sudo().search([('token', '=', token)], limit=1)
-        if  client_id:
+        if client_id:
             _logger.error("Token is not valid!")
             return True
-        return super(AccessToken,self)
+        return super(AccessToken, self)
