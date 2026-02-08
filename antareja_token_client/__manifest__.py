4a74174ac6671b0ccec6906c404c70c219760c5f
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-02-08 15:52:53 +0700

    antareja_token_client 13.0.0.0.3

--- antareja_token_client/__manifest__.py
+++ antareja_token_client/__manifest__.py
@@ -10,8 +10,8 @@
     'author': "Agus Muhammad Ramdan",
     'website': "http://www.yourcompany.com",
     'category': 'API',
-    'version': '13.0.0.0.1',
-    'depends': ['base', 'antareja_base','antareja_token', 'cni_api' ,'rest_api'],
+    'version': '13.0.0.0.3',
+    'depends': ['base', 'antareja_base', 'antareja_token', 'cni_api'],
     # always loaded
     'data': [],
 }
