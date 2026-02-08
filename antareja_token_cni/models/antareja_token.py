007623a34165a854531c9d9e985192f50a37c6db
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-02-08 11:02:58 +0700

    antareja_token_cni 13.0.0.0.0

---
+++ antareja_token_cni/models/antareja_token.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from odoo import models, fields
+
+_logger = logging.getLogger(__name__)
+
+
+class AccessToken(models.Model):
+    _inherit = 'antareja.token'
+
+    def validate(self, access_token, refresh_token=False):
+        try:
+            result = super(AccessToken, self).validate(access_token, refresh_token=refresh_token)
+        except Exception as e:
+            result = None
+        if not result and not refresh_token:
+            access_token_data = self.env['oauth.access_token'].sudo().search([('token', '=', access_token)], order='id DESC', limit=1)
+            if access_token_data._get_access_token_google(employee_id=access_token_data.employee_id.id) != access_token:
+                user = access_token_data.employee_id.user_id
+                if user:
+                    epoch_unix = None
+                    if access_token_data.expires:
+                        epoch_timestamp = fields.Datetime.to_datetime(access_token_data.expires).timestamp()
+                        epoch_unix = int(epoch_timestamp)
+                    result = {
+                        'uid': user.id,
+                        'login': user.login,
+                        'sub': user.login,
+                        'email': user.email,
+                        'exp': epoch_unix,
+                        'token': access_token,
+                    }
+        return result
