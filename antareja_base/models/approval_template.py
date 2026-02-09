3607e2af5cbf98da7eda93be940c56dd970f1628
agus <agus@antarejasolusi.com>
2026-02-09 09:43:10 +0700

    antareja_base

--- antareja_base/models/approval_template.py
+++ antareja_base/models/approval_template.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # v16 from odoo import tools,Command
-from odoo import models, fields, api,tools,Command
+from odoo import models, fields, api, tools, Command
 from odoo.exceptions import UserError, ValidationError
 from odoo.tools import float_compare
 from odoo.tools.safe_eval import safe_eval, test_python_expr
