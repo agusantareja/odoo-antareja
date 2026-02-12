dc0098ddd78eedfe7206d5840bb1561a959b753a
agus <agus@antarejasolusi.com>
2026-02-12 14:43:39 +0700

    antareja_notification_admin_user 13.0.1.0.3

--- antareja_notification_admin_user/models/approval_task.py
+++ antareja_notification_admin_user/models/approval_task.py
@@ -9,14 +9,6 @@ _logger = logging.getLogger(__name__)
 class ApprovalTask(models.Model):
     _inherit = 'approval.task'
 
-    def get_users_for_approval(self,**kwargs):
-        users = super(ApprovalTask, self).get_users_for_approval(**kwargs)
-        return users and users.filtered(lambda u: not u.admin_user)
-
     def get_users_for_notification(self,**kwargs):
         users = super(ApprovalTask, self).get_users_for_notification(**kwargs)
-        return users and users.filtered(lambda u: not u.admin_user)
-
-    def get_users_for_mobile_approval(self,**kwargs):
-        users = super(ApprovalTask, self).get_users_for_mobile_approval(**kwargs)
         return users and users.filtered(lambda u: not u.admin_user)
