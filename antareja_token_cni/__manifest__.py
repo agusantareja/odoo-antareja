28bfba048b5f67e8cd40f9811afe56e58ed7d2fa
agus <agus@antarejasolusi.com>
2026-02-13 15:49:29 +0700

    antreja_token_cni

---
+++ antareja_token_cni/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Token CNI",
+    'summary': """
+        Token Client
+    """,
+    'description': """
+    """,
+    'author': "Agus Muhammad Ramdan",
+    'website': "http://www.yourcompany.com",
+    'category': 'API',
+    'version': '13.0.0.0.0',
+    'depends': ['base', 'antareja_base', 'antareja_token', 'rest_api'],
+    # always loaded
+    'data': [],
+}
