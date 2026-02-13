dc1296471d1bda3389b3136e8a204ea4870eb0cd
agus <agus@antarejasolusi.com>
2026-02-13 11:12:45 +0700

    antareja_approval_ceria_mobile_local

--- antareja_approval_ceria_mobile_local/__manifest__.py
+++ antareja_approval_ceria_mobile_local/__manifest__.py
@@ -2,12 +2,12 @@
 
 {
     'name': 'Antareja || Notification Ceria Mobile Local',
-    'version': '13.0.1.0.1',
+    'version': '13.0.1.0.3',
     "category": "Extra Tools",
     "license" : "LGPL-3",
     'author': "Agus Muhammad Ramdan, Antareja Sinergi Sejahtera",
     'description': """Ceria mobile yang ada di intra""",
-    'depends': ['base','antareja_base', 'ceria_mobile_approval'],
+    'depends': ['base', 'antareja_notification_ceria_mobile_intra', 'ceria_mobile_approval'],
     'data': [
         'views/res_config_settings_views.xml',
     ],
