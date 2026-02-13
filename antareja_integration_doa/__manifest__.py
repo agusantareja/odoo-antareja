3b3b68021e91ff342174710314472036a3327e1f
agus <agus@antarejasolusi.com>
2026-02-13 10:01:32 +0700

    antareja_integration_doa

---
+++ antareja_integration_doa/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "DOA || Client",
+    'summary': """
+        DOA Client 
+        """,
+    'description': """
+    """,
+    'author': "Agus Muhammad Ramdan",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '13.0.1',
+    'depends': ['base','mail', 'amr_data_sync'],
+    # always loaded
+    'data': [
+        'data/external_sync_server.xml',
+    ],
+}
