df3e42ce3556b6cf88696a9638d4379a92aa9d50
agus <agus@antarejasolusi.com>
2026-02-13 10:00:02 +0700

    antareja_doa

--- antareja_doa/models/res_users.py
+++ antareja_doa/models/res_users.py
@@ -139,3 +139,17 @@ class ResUsers(models.Model):
             if delegatee_ids:
                 return self.browse(delegatee_ids)
         return self.browse()
+
+    def get_notification_user_ids(self, company_id=None):
+        """
+        Override method to return the user itself as a notification recipient.
+        This is useful for cases where the user needs to receive notifications
+        about their own actions or changes.
+        """
+
+        uid = self.id
+        if uid and uid != self._uid:
+            self = self.with_user(uid)
+
+        return self.env['user.delegate'].get_notification_user_ids(user_ids=[self._uid],company_id=company_id)
+
