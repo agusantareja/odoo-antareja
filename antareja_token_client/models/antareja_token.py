a86e07db9626670681d3b138891bc7d993393b49
agus <agus@antarejasolusi.com>
2026-02-13 15:41:01 +0700

    antareja_token_client

--- antareja_token_client/models/antareja_token.py
+++ antareja_token_client/models/antareja_token.py
@@ -10,10 +10,10 @@ _logger = logging.getLogger(__name__)
 class AccessToken(models.Model):
     _inherit = 'antareja.token'
 
-    def client_token_validation(self,token):
+    def client_token_validation(self, token):
         # client_api
         client_id = self.env['client.api'].sudo().search([('token', '=', token)], limit=1)
-        if  client_id:
+        if client_id:
             _logger.error("Token is not valid!")
             return True
-        return super(AccessToken,self)
+        return super(AccessToken, self).client_token_validation(token)
