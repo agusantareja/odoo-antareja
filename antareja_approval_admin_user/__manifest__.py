dbce5be630fc49ca223a83d691ed9195454170f4
agus <agus@antarejasolusi.com>
2026-02-13 15:29:44 +0700

    antareja_approval_admin_user 13.0.2.0.4

---
+++ antareja_approval_admin_user/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Antareja || Ignore Admin User on Approval',
+    'version': '13.0.1.0.2',
+    "category": "Extra Tools",
+    "license": "LGPL-3",
+    'author': "Agus Muhammad Ramdan, Antareja Sinergi Sejahtera",
+    'description': """  Ignore Admin User on Approval
+    """,
+    'depends': ['base', 'mail', 'antareja_base', 'auth_admin_sso'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False
+}
---
+++ antareja_approval_admin_user/models/approval_task.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class ApprovalTask(models.Model):
+    _inherit = 'approval.task'
+
+    def get_users_for_approval(self,**kwargs):
+        users = super(ApprovalTask, self).get_users_for_approval(**kwargs)
+        return users and users.filtered(lambda u: not u.admin_user)
+
+    def get_users_for_mobile_approval(self,**kwargs):
+        users = super(ApprovalTask, self).get_users_for_mobile_approval(**kwargs)
+        return users and users.filtered(lambda u: not u.admin_user)
