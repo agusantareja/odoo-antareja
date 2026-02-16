73b628730788625fcbd29668d277981e38771dbe
agus <agus@antarejasolusi.com>
2026-02-16 15:09:17 +0700

    ceria_mobile_approval

---
+++ ceria_mobile_approval/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name':"Ceria Mobile Approval",
+    'version':'13.0.1.0.1',
+    'depends':['base', 'ceria_mobile', 'antareja_base', 'antareja_token'],
+    'website':"",
+    'description':"""Ceria Mobile Approval""",
+    'data': [
+        'security/ir.model.access.csv',
+        'views/ceria_mobile_approval_views.xml',
+        'views/menuitem.xml',
+    ],
+}
---
+++ ceria_mobile_approval/controllers/main.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from odoo import api, http, SUPERUSER_ID, _
+from odoo.http import request
+from odoo import registry as registry_get
+#from odoo.addons.cni_api.controllers.main import check_valid_token
+from odoo.addons.antareja_base.tools.rest import valid_response,invalid_response,get_body_json
+from odoo.addons.antareja_token.tools.utils import check_token_authorization
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+_logger = logging.getLogger(__name__)
+
+
+class MainController(http.Controller):
+
+    @http.route('/api/intra/mobile/approval', methods=['POST'], type='http', auth='none', csrf=False)
+    @check_token_authorization
+    def post_mobile_approval(self,**post):
+        data = post or get_body_json()
+        if not data:
+            return invalid_response(400,"no_data","enpty data")
+        new_registry = registry_get(request.session.get('db'))
+        with new_registry.cursor() as cr:
+            env = api.Environment(cr, SUPERUSER_ID, {})
+            result = env['ceria.mobile.approval'].api_create_request(**data)
+
+        return valid_response(200,result)
+
+    @http.route('/api/intra/mobile/approval', methods=['GET'], type='http', auth='none', csrf=False)
+    @check_token_authorization
+    def get_mobile_approval(self,**post):
+        data = post
+        new_registry = registry_get(request.session.get('db'))
+        with new_registry.cursor() as cr:
+            env = api.Environment(cr, SUPERUSER_ID, {})
+            result = env['ceria.mobile.approval'].api_get_approvals(**data)
+        return valid_response(200,result)
+
+    @http.route('/api/intra/mobile/approval/distinct', methods=['GET'], type='http', auth='none', csrf=False)
+    @check_token_authorization
+    def get_mobile_approval_distinct(self, **post):
+        data = post
+        new_registry = registry_get(request.session.get('db'))
+        with new_registry.cursor() as cr:
+            env = api.Environment(cr, SUPERUSER_ID, {})
+
+            if 'user_email' in data:
+                domain = [('user_id.partner_id.email', '=', data['user_email'])]
+            else:
+                domain = []
+
+            if 'field_select' not in data:
+                return invalid_response(400, "field_select parameter not found", "Invalid data query")
+            else:
+                field_select = data['field_select']
+
+            result_group = env['ceria.mobile.approval'].read_group(
+                domain=domain,
+                fields=[field_select],
+                groupby=[field_select],
+                lazy=False
+            )
+            result = [str(rec.get(field_select, "")) for rec in result_group]
+        return valid_response(200, result)
