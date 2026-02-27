7123232b4221c85a3c47ff81c89747441f16e4c9
agus <agus@antarejasolusi.com>
2026-02-27 08:55:14 +0700

    ahda_dynamic_whatsapp_client

--- ahda_dynamic_whatsapp_client/models/whatsapp_template.py
+++ ahda_dynamic_whatsapp_client/models/whatsapp_template.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+from odoo.tools.safe_eval import safe_eval
 import ast
 import json
 import logging
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError, ValidationError
-from odoo.tools.safe_eval import safe_eval
 import random
 import re
 import requests
