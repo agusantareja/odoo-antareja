cee7962b28f97923f7e7a6ea81689a468aa74f83
agus <agus@antarejasolusi.com>
2026-03-18 15:30:17 +0700

    antareja_approval_whatsapp 13.0.1

---
+++ antareja_approval_whatsapp/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Notification Whatapp Client",
+    'summary': """Notification Whatapp Client and Integration """,
+    'description': """Notification Whatapp Client and Integration""",
+    'author': "Agus Muhammad Ramdan",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '13.0.1',
+    'depends': ['base', 'mail', 'antareja_notification_whatsapp'],
+    # always loaded
+    'data': [
+        'data/notification_template_data.xml',
+    ],
+}
