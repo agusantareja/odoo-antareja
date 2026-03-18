d615fe11be4cae08cfe12690907f55a3047d27f1
agus <agus@antarejasolusi.com>
2026-03-18 16:23:13 +0700

    antareja_token_oauth

---
+++ antareja_token_oauth/models/antareja_token.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from odoo import models
+
+_logger = logging.getLogger(__name__)
+
+
+class AccessToken(models.Model):
+    _inherit = 'antareja.token'
+
+    def validate(self, token, refresh_token=False):
+        if not refresh_token:
+            res = self.env['res.users'].sudo().search([('oauth_access_token', '=', token)], limit=1)
+            if res and res.oauth_provider_id:
+                payload = res._auth_oauth_validate(int(res.oauth_provider_id), token)
+                payload['uid'] = res.id
+                payload['username'] = res.login
+                return payload
+        return super(AccessToken, self).validate(token, refresh_token=refresh_token)
