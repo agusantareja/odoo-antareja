1185cead4db8924d180ad0b4168a4476d4586b62
agus <agus@antarejasolusi.com>
2026-03-18 16:22:56 +0700

    antareja_doa

--- antareja_doa/models/__init__.py
+++ antareja_doa/models/__init__.py
@@ -3,3 +3,6 @@
 from . import user_delegation
 from . import res_users
 from . import res_config_settings
+from . import approval_task
+from . import approval_task_line_mixin
+from . import approval_audit_log
