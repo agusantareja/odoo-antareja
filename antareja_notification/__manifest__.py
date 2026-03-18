a0d5de4c370c4d2ab3c6f6860bea2e962174ad7d
agus <agus@antarejasolusi.com>
2026-03-18 16:32:57 +0700

    antareja_notification 13.0.1.0.6

--- antareja_notification/__manifest__.py
+++ antareja_notification/__manifest__.py
@@ -13,6 +13,7 @@ disediakan untuk penyederhanaan pengiriman email/WhatsApp.
     'depends': ['base', 'mail', 'antareja_base',],
     'data': [
         'security/ir.model.access.csv',
+        'views/approval_task_views.xml',
         'views/notification_template_views.xml',
         'views/notification_log_views.xml',
         'views/approval_template_views.xml',
