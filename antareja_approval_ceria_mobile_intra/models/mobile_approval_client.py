abd5b74435856713f36281870a75d3f682f903b6
agus <agus@antarejasolusi.com>
2026-04-24 16:39:20 +0700

    antareja_approval_ceria_mobile_intra

--- antareja_approval_ceria_mobile_intra/models/mobile_approval_client.py
+++ antareja_approval_ceria_mobile_intra/models/mobile_approval_client.py
@@ -46,6 +46,7 @@ class MobileApprovalClient(models.Model):
     def get_application_name(self):
         return self.env['ir.config_parameter'].sudo().get_param('antareja.application_name')
 
+    @api.model
     def get_mobile_approval_path(self):
         return "/api/intra/mobile/approval"
 
