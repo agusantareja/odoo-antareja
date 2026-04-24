940ae89b8644e154dba0da2282021d783548de3f
agus <agus@antarejasolusi.com>
2026-04-24 16:38:30 +0700

    ceria_mobile_notification_test

---
+++ ceria_mobile_notification_test/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name':"Ceria Mobile Notification",
+    'version':'13.0.1.0.1',
+    'depends':['base','cni_api','ceria_mobile','firebase_config'],
+    'website':"",
+    'description':"""Ceria Mobile Notification""",
+    'data': [
+        'views/res_config_settings.xml',
+    ],
+}
---
+++ ceria_mobile_notification_test/models/res_config_settings.py
@@ -0,0 +1,6 @@
+from odoo import models, fields
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    ceria_mobile_notification_device_test_token = fields.Char(string='Device Test Token', config_parameter='ceria_mobile_notification.device_test_token')
