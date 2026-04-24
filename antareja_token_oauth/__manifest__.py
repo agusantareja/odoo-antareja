60372bd37822ddf3d61fa39327e39fecda2a67b2
agus <agus@antarejasolusi.com>
2026-04-24 16:37:59 +0700

    antareja_token_oauth

--- antareja_token_oauth/__manifest__.py
+++ antareja_token_oauth/__manifest__.py
@@ -1,18 +1,12 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': "Token OAuth",
-    'summary': """
-        Token OAuth
-        """,
-    'description': """
-    """,
+    'name': "OAuth Token",
+    'summary': "OAuth Token",
+    'description': "",
     'author': "Agus Muhammad Ramdan",
     'website': "http://www.yourcompany.com",
     'category': 'API',
-    'version': '13.0.0.0.0',
+    'version': '13.0.0.0.2',
     'depends': ['base', 'antareja_base','antareja_token','cni_auth_oauth' ],
-    # always loaded
-    'data': [
-    ],
 }
