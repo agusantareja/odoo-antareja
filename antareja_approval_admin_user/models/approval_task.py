ade75e592e4b013d7a03b6eb4730fc8fae9e7be9
agus <agus@antarejasolusi.com>
2026-04-29 17:20:42 +0700

    antareja_approval_admin_user

--- antareja_approval_admin_user/models/approval_task.py
+++ antareja_approval_admin_user/models/approval_task.py
@@ -12,8 +12,12 @@ class ApprovalTask(models.Model):
 
     def get_users_for_approval(self, **kwargs):
         users = super(ApprovalTask, self).get_users_for_approval(**kwargs)
-        return users.filtered(lambda u: not u.admin_user)
+        if users and isinstance(users, models.BaseModel):
+            return users.filtered(lambda u: not u.admin_user)
+        return users
 
     def get_users_for_mobile_approval(self, **kwargs):
         users = super(ApprovalTask, self).get_users_for_mobile_approval(**kwargs)
-        return users.filtered(lambda u: not u.admin_user)
+        if users and isinstance(users, models.BaseModel):
+            return users.filtered(lambda u: not u.admin_user)
+        return users
