1cdc029a940b5a361bb751ca087f704bcca747e0
agus <agus@antarejasolusi.com>
2026-05-06 15:41:41 +0700

    antareja_doa

--- antareja_doa/models/approval_audit_log.py
+++ antareja_doa/models/approval_audit_log.py
@@ -5,4 +5,4 @@ class ApprovalAuditLog(models.Model):
     _inherit = 'approval.audit.log'
     delegatee_user_id = fields.Many2one('res.users', string="Acting User")
     delegator_user_id = fields.Many2one('res.users', string="On Behalf Of")
-    user_delegation_id = fields.Many2one('user.delegation', string="Delegate Rule")
+    user_delegation_id = fields.Many2one('user.delegation', string="Delegate Rule", ondelete='set null')
