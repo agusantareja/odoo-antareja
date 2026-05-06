3b68958df674a1a46a30583d179723add4d6204f
agus <agus@antarejasolusi.com>
2026-05-06 13:50:59 +0700

    antareja_notification_admin_user

--- antareja_notification_admin_user/models/res_users.py
+++ antareja_notification_admin_user/models/res_users.py
@@ -13,4 +13,4 @@ class ResUsers(models.Model):
     def get_users_for_notification(self, company=None):
         if not self:
             return self
-        return super(ResUsers,self.filtered(lambda u: not u.admin_user)).get_users_for_notification(company=company)
+        return super(ResUsers,self.filtered(lambda u: not u.admin_user and u.id not in [1,2])).get_users_for_notification(company=company)
