b90f32fd6abcc558789017fc4547244632073766
agus <agus@antarejasolusi.com>
2026-05-12 13:36:22 +0700

    MBL-00008: tambahkan informasi json yang diterima kedalam data exclude beberapa key

---
+++ antareja_doa_data_event/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Event Data Config DOA for Sync',
+    'version': '13.0.0.0.0',
+    'category': 'Sync',
+    'author': 'Agus Muhammad Ramdan',
+    'website': 'https://agus.ramdan.tech',
+    'summary': 'Event Data Config DOA for Sync',
+    'description': "Event Data Config DOA for Sync",
+    'depends': ['base', 'amr_data_event', ],
+    'data': ['data/event_data.xml'],
+    'installable': True,
+    'auto_install': False,
+}
