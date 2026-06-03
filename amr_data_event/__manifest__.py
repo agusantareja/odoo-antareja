2016e5c69398222fa8d41fd5cb4109f62aefd6b5
agus <agus@antarejasolusi.com>
2026-06-03 11:54:20 +0700

    amr_data_event

--- amr_data_event/__manifest__.py
+++ amr_data_event/__manifest__.py
@@ -11,7 +11,7 @@
     "license": "AGPL-3",
     'website': "https://agus.ramdan.tech",
     'category': 'tools',
-    'version': '13.0.0.0.2',
+    'version': '13.0.0.0.3',
     'depends': ['base'],
     # always loaded
     'data': [
--- amr_data_event/models/data_event_mixin.py
+++ amr_data_event/models/data_event_mixin.py
@@ -1,4 +1,7 @@
+
 import logging
+
+from odoo import models
 
 _logger = logging.getLogger(__name__)
 
