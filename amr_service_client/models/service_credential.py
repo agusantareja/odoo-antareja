bccbc027281235c0ddca97e2dd7e62f15563dec0
agus <agus@antarejasolusi.com>
2026-06-23 13:05:28 +0700

    amr_service_client 13.0.0.0.3

---
+++ amr_service_client/models/service_credential.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from odoo import api, fields, models, tools
+from odoo.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
+
+
+class ServiceCredential(models.Model):
+    _name = "service.credential"
+    _description = "Credential"
+
+    name = fields.Char(required=True)
+    source = fields.Selection(
+        [
+            ("database", "Database"),
+            ("file_path", "File Path"),
+            ("env_json", "Environment JSON"),
+            ("env_path", "Environment Path"),
+            ("config_path", "Config Path"),
+            ("system_parameter_json", "System Parameter JSON"),
+            ("system_parameter_path", "System Parameter Path"),
+        ],
+        required=True,
+    )
+    reference = fields.Char()
+    credential_json = fields.Text()
+    scopes = fields.Char(help="Comma-separated scopes")
+
+    def action_config_credential(self):
+        self.ensure_one()
+
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Config Credential",
+            "res_model": "service.credential.wizard",
+            "view_mode": "form",
+            "target": "new",
+            "context": {
+                "default_endpoint_id": self.id,
+            }
+        }
+
+    def action_test_credential(self):
+        self.ensure_one()
+        try:
+            loader = self.env["service.credential.loader"]
+            credential_data = loader.load_credential(self)
+            provider = self.env["service.auth.factory"].create_service_auth(credential_data)
+            token = provider._request_token()
+            _logger.debug(f"Credential {self.name} , token: {token}...")
+            if 'access_token' in token:
+                self.env['amr.resource.helper'].decode(token['access_token'])
+            return {
+                "type": "ir.actions.client",
+                "tag": "display_notification",
+                "params": {
+                    "title": "Success",
+                    "message": f"Credential {self.name} is valid",
+                    "type": "success",
+                }
+            }
+        except Exception as e:
+            _logger.error(f"Credential {self.name} is invalid: {e}")
+            raise UserError(f"Credential is invalid: {e}")
